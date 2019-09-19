dfbe63c6826192f85e5879d619846893e90d78db
gratien <gratien@gratien-ThinkPad-W540>
2019-09-19 17:08:53 +0200

    new commit

--- DCAfric-war/web/html/js/index_script.js
+++ DCAfric-war/web/html/js/index_script.js
@@ -4,4 +4,20 @@
  * and open the template in the editor.
  */
 var stringQuery=new Array();
-$
+$(function(){
+    if(stringQuery.length===0){
+        if(window.location.search.split('?').length>1){
+            var params=window.location.search.split('?')[1].split('&');
+            for(var i=0;i<params.length;i++){
+              var keyparam=params[i].split('=')[0];
+              var valueparam=decodeURIComponent(params[i].split('=')[1]);
+              stringQuery[keyparam]=valueparam;
+            }
+            
+        }
+    }
+    if(stringQuery["prenom"]!==null && stringQuery["nom"]!==null){
+        var data="<b> Bonjour : "+stringQuery["prenom"]+" "+stringQuery["nom"]+"</b>";
+        $("#lblUser").html(data);
+    }
+});
